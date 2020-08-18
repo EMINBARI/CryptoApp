befae61c99711cd7e9d5aa7c71d7b44339c18fbe
Emin <38878208+EMINBARI@users.noreply.github.com>
2020-08-18 10:19:48 +0300

    Initial Commit

---
+++ CryptoApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Emin Bari on 18.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
